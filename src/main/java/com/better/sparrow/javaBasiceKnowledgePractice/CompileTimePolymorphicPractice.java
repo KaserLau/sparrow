f55bb33f0ec87f3f52564c6f72cc3a1d7f8ef98b
kaser.lau <kaser.lau@outlook.com>
2018-12-09 19:47:36 +0800

    运行时多态例子

---
+++ src/main/java/com/better/sparrow/javaBasiceKnowledgePractice/CompileTimePolymorphicPractice.java
@@ -0,0 +1,20 @@
+package com.better.sparrow.javaBasiceKnowledgePractice;
+
+/**
+ * @Author : Kaser.Lau
+ * @Description :
+ * @Create Date : 7:34 PM 2018/12/9
+ * @Modified By :
+ */
+public class CompileTimePolymorphicPractice {
+    public void testCompileTimePolymorphic(){
+        SuperClass superClass = new SuperClass();
+        System.out.println("获取superClass的books参数"+ superClass.books);
+        superClass.base();
+        superClass.test();
+        SubClass subClass = new SubClass();
+        System.out.println("获取SubClass的books参数"+ subClass.books);
+        subClass.test();
+        subClass.sub();
+    }
+}
